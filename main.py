96293a71fd1d4b35fe8b85d4d531e9219a8bc2b5
Steve Gossip <stephengossip@gmail.com>
2022-02-02 19:32:03 +0000

    refactored from data to listings and accessed the google form url using selenium

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from bs4 import BeautifulSoup
+import selenium
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 import requests
 
 RIGHTMOVE_URL = 'https://www.rightmove.co.uk/property-to-rent/find.html?searchType' \
@@ -11,28 +14,30 @@ RIGHTMOVE_URL = 'https://www.rightmove.co.uk/property-to-rent/find.html?searchTy
 GOOGLE_FORM_URL = 'https://docs.google.com/forms/d/e/1FAIpQLSdIYPzH1Aim7R4IFqI4BQeVIbkGcb8pTfEeXbJF-' \
                   'R9KQ5Rtpw/viewform?usp=sf_link'
 
+CHROME_DRIVER_PATH = '/Users/admin/Development/chromedriver'
+
+# BEAUTIFULSOUP
+# FIND ALL RESULTING LISTINGS DOM ELEMENTS
 response = requests.get(RIGHTMOVE_URL)
 rightmove_listings = response.text
 soup = BeautifulSoup(rightmove_listings, 'html.parser')
+property_results = soup.find_all(name='div', class_="l-searchResult")
 
-print(soup.prettify())
 
-all_properties = soup.find_all(name='div', class_="l-searchResult")
-
-print(len(all_properties))
-
-data = []
-for property in all_properties:
+listings = []
+for result in property_results:
     new_dict = {}
-    price = property.find(name="span", class_="propertyCard-priceValue")
-    address = property.find(name="address", class_="propertyCard-address")
-    link = property.find(name="a", class_="propertyCard-img-link")['href']
+    price = result.find(name="span", class_="propertyCard-priceValue")
+    address = result.find(name="address", class_="propertyCard-address")
+    link = result.find(name="a", class_="propertyCard-img-link")['href']
     new_dict["price"] = price.text.split(" ")[0]
     new_dict["address"] = address.text.replace("\n", "")
     new_dict["link"] = f"https://www.rightmove.co.uk/{link}"
+    listings.append(new_dict)
+print(listings)
+# SELENIUM
+# ACCESS GOOGLE FORM AND FILL IN FOR EACH LISTING
+driver = webdriver.Chrome(executable_path=CHROME_DRIVER_PATH)
+driver.get(GOOGLE_FORM_URL)
 
-    print(new_dict)
-    data.append(new_dict)
 
-
-print(data[0])
